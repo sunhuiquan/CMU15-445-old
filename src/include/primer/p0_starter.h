037c9f3214b4447da1a7488fac987dce0f1f0fcd
sunhuiquan <sunhuiquan@yeah.net>
2022-05-12 18:11:47 +0800

    add Matrix and RowMatrix

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -35,7 +35,7 @@ class Matrix {
    * @param cols The number of columns
    *
    */
-  Matrix(int rows, int cols) {}
+  Matrix(int rows, int cols) : rows_(rows), cols_(cols) { liner_ = new T[rows * cols]; }
 
   /** The number of rows in the matrix */
   int rows_;
@@ -95,7 +95,7 @@ class Matrix {
    * Destroy a matrix instance.
    * TODO(P0): Add implementation
    */
-  virtual ~Matrix() = default;
+  virtual ~Matrix() { delete[] linear_; }
 };
 
 /**
@@ -112,19 +112,24 @@ class RowMatrix : public Matrix<T> {
    * @param rows The number of rows
    * @param cols The number of columns
    */
-  RowMatrix(int rows, int cols) : Matrix<T>(rows, cols) {}
+  RowMatrix(int rows, int cols) : Matrix<T>(rows, cols) {
+    data_ = new T *[rows];
+    for (int i = 0; i < rows; ++i) {
+      data[i] = new T[cols];
+    }
+  }
 
   /**
    * TODO(P0): Add implementation
    * @return The number of rows in the matrix
    */
-  int GetRowCount() const override { return 0; }
+  int GetRowCount() const override { return rows_; }
 
   /**
    * TODO(P0): Add implementation
    * @return The number of columns in the matrix
    */
-  int GetColumnCount() const override { return 0; }
+  int GetColumnCount() const override { return cols_; }
 
   /**
    * TODO(P0): Add implementation
@@ -139,7 +144,10 @@ class RowMatrix : public Matrix<T> {
    * @throws OUT_OF_RANGE if either index is out of range
    */
   T GetElement(int i, int j) const override {
-    throw NotImplementedException{"RowMatrix::GetElement() not implemented."};
+    if (i * cols_ + j >= rows_ * cols_) {
+      throw Exception(ExceptionType::OUT_OF_RANGE, "Index is out of range.");
+    }
+    return linear_[i * cols_ + j];
   }
 
   /**
@@ -152,7 +160,12 @@ class RowMatrix : public Matrix<T> {
    * @param val The value to insert
    * @throws OUT_OF_RANGE if either index is out of range
    */
-  void SetElement(int i, int j, T val) override {}
+  void SetElement(int i, int j, T val) override {
+    if (i * cols_ + j >= rows_ * cols_) {
+      throw Exception(ExceptionType::OUT_OF_RANGE, "Index is out of range.");
+    }
+    linear_[i * cols_ + j] = val;
+  }
 
   /**
    * TODO(P0): Add implementation
@@ -166,7 +179,14 @@ class RowMatrix : public Matrix<T> {
    * @throws OUT_OF_RANGE if `source` is incorrect size
    */
   void FillFrom(const std::vector<T> &source) override {
-    throw NotImplementedException{"RowMatrix::FillFrom() not implemented."};
+    int sz = rows_ * cols_;
+    if (source.size() != sz) {
+      throw Exception(ExceptionType::OUT_OF_RANGE, "Index is out of range.");
+    }
+
+    for (int i = 0; i < sz; ++i) {
+      linear_[i] = source[i];
+    }
   }
 
   /**
@@ -174,7 +194,12 @@ class RowMatrix : public Matrix<T> {
    *
    * Destroy a RowMatrix instance.
    */
-  ~RowMatrix() override = default;
+  ~RowMatrix() override {
+    for (int i = 0; i < rows_; ++i) {
+      delete data[i];
+    }
+    delete[] data;
+  }
 
  private:
   /**
